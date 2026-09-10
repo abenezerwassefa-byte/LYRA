840452b1434dd302258cd45541350c97c7ce5d80
Abenezer W. Assefa <abenezer.w.assefa@gmail.com>
2026-09-10 10:12:55 +0300

    Delete transcription directory

--- transcription/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Recording(models.Model):
-    audio = models.FileField(upload_to="recordings")
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, default="pending")
-# Create your models here.
